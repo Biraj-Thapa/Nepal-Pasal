e419ef010ff8480da2eebc9cb4d1a4b28559a620
Biraj Thapa <birajthapa983@gmail.com>
2024-06-05 16:25:40 +0545

    added userSchema

--- server/index.js
+++ server/index.js
@@ -1,8 +1,24 @@
 const express = require('express')
 const connection= require('./src/db/connection')
 const app = express()
-const port = 4000
+app.use(express.json())
+const mongoose = require('mongoose')
+const { Schema } = mongoose;
+require('dotenv').config()
+const port = process.env.PORT
 connection()
+const userSchema = new Schema({
+  phoneNumber: String, 
+  fullName: String,
+  email: String,
+  password: String,
+  role: {
+    type: String,
+    enum : ['admin','user'],
+    default: 'user'
+  },
+});
+const User = mongoose.model('User', userSchema);
 
 
 app.get('/', (req, res) => {
